e88ee955be3c8a5891c963107874daf6bc19b008
maikaaa7 <mendiolm6967@student.faytechcc.edu>
2025-11-05 23:18:57 -0500

    First Python Assignment
    Taking input from user and printing the output.

---
+++ P1LAB1_MendiolaMichaelynn.py
@@ -0,0 +1,8 @@
+# Michaelynn Mendiola
+# P1LAB1_MendiolaMichaelynn.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
