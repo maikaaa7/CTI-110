40ac96eaad00128a2b0b3e758ecf0e220ec12ccf
maikaaa7 <mendiolm6967@student.faytechcc.edu>
2025-11-22 13:51:22 -0500

    Python Assignment

---
+++ P3LAB_MendiolaMichaelynn.py
@@ -0,0 +1,63 @@
+# Michaelynn Mendiola
+# 11/16/2025
+# P3LAB
+# This program allows the user to enter a money (float) value with two places after the decimal.
+
+#Get value from user
+change = float(input("Enter an amount of money as a float: $"))
+
+#If the input is 0, stop immediately
+if change == 0:
+    print("No change")
+    exit()
+print(f"Change amount: {change}")        
+
+#Converting the value to an integer
+change = round(change * 100)
+
+#Determine how many coins are needed
+num_dollars = change // 100
+change = change - (num_dollars * 100)
+
+num_quarters = change // 25
+change = change  - (num_quarters * 25)
+
+num_dimes = change // 10
+change = change - (num_dimes * 10)
+
+num_nickels = change // 5
+change = change - (num_nickels * 5)
+
+num_pennies = change
+
+if num_dollars > 0:
+    if num_dollars == 1:
+        print(f"{num_dollars} Dollar")
+    else:
+        print(f"{num_dollars} Dollars")
+
+if num_quarters > 0:
+    if num_quarters == 1:
+        print(f"{num_quarters} Quarter")
+    else:
+        print(f"{num_quarters} Quarters")
+
+if num_dimes > 0: 
+    if num_dimes == 1:
+        print(f"{num_dimes} Dime")
+    else:
+        print(f"{num_dimes} Dimes")
+
+if num_nickels > 0: 
+    if num_nickels == 1:
+        print(f"{num_nickels} Nickel")
+    else:
+        print(f"{num_nickels} Nickels")
+
+if num_pennies > 0: 
+    if num_pennies == 1:
+        print(f"{num_pennies} Penny")
+    else:
+        print(f"{num_pennies} Pennies")           
+
+  
